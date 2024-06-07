1b21f361e542bc3c31fc800f73a765e3c29318f4
Yoon Joo Woo <yjdream86@gmail.com>
2024-06-06 22:21:58 -0400

    frame of the signin page renders on a browser

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import Signin from './components/Signin';
 
 const App = () => {
   return (
-    <>
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    </>
-
+    <div>
+      <Signin />
+    </div>
   );
 };
 
