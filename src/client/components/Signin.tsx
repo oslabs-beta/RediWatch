9120139c56528078c7db60ffb8e2f2d3639ebb59
Yoon Joo Woo <yjdream86@gmail.com>
2024-06-08 11:37:00 -0400

    color theme and small features changed

--- src/client/components/Signin.tsx
+++ src/client/components/Signin.tsx
@@ -96,24 +96,25 @@ export default function SignInSide() {
                 id="password"
                 autoComplete="current-password"
               />
-              <FormControlLabel
+              {/* <FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
                 label="Remember me"
-              />
+              /> */}
               <Button
                 type="submit"
                 fullWidth
                 variant="contained"
+                color='secondary'
                 sx={{ mt: 3, mb: 2 }}
               >
                 Sign In
               </Button>
-              <Grid container>
-                <Grid item xs>
+              <Grid container justifyContent="flex-end">
+                {/* <Grid item xs>
                   <Link href="#" variant="body2">
                     Forgot password?
                   </Link>
-                </Grid>
+                </Grid> */}
                 <Grid item>
                   <Link component={RouterLink} to='/signup' variant="body2">
                     {"Don't have an account? Sign Up"}
--- src/client/components/Signup.tsx
+++ src/client/components/Signup.tsx
@@ -102,17 +102,18 @@ export default function SignUp() {
                   autoComplete="new-password"
                 />
               </Grid>
-              <Grid item xs={12}>
+              {/* <Grid item xs={12}>
                 <FormControlLabel
                   control={<Checkbox value="allowExtraEmails" color="primary" />}
                   label="I want to receive inspiration, marketing promotions and updates via email."
                 />
-              </Grid>
+              </Grid> */}
             </Grid>
             <Button
               type="submit"
               fullWidth
               variant="contained"
+              color="secondary"
               sx={{ mt: 3, mb: 2 }}
             >
               Sign Up
