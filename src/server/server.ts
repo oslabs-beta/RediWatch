a86ba5d907b109b8bc4a422aa9be3e9c9c7e9c98
Yoon Joo Woo <yjdream86@gmail.com>
2024-07-02 21:16:55 -0400

    prometheus/grafana setup in my local machine

--- src/server/server.ts
+++ src/server/server.ts
@@ -7,37 +7,30 @@ const PORT = 3001;
 app.use(express.json());
 
 
+
 // const redisClient = createClient({
-//     password: 'KptlZCYma2TJ1hIwR4ZkRQP0Uu7Cuq4k',
-//     socket: {
-//         host: 'redis-17407.c278.us-east-1-4.ec2.redns.redis-cloud.com',
-//         port: 17407
+//     url: 'redis://default:J4qW0Kf6udoPHKmthBviCYfGXE5ommO2@redis-11565.c274.us-east-1-3.ec2.redns.redis-cloud.com:11565'
+// })
+
+// redisClient.on('error', (err) => console.error('Redis Client Error', err));
+
+// // Connect to Redis
+// redisClient.connect();
+
+
+// // convert Redis INFO metrics to Prometheus format
+// const prometheusFormat = (metrics: { [key: string]: string}): string => {
+//     let output = '';
+//     for (const [key, value] of Object.entries(metrics)) {
+//         // Attempt to parse the value as a float
+//         const floatValue = parseFloat(value);
+//         if (!isNaN(floatValue)) {
+//             output += `# TYPE redis_${key} gauge\n`;
+//             output += `redis_${key} ${floatValue}\n`;
+//         }
 //     }
-// });
-
-const redisClient = createClient({
-    url: 'redis://default:KptlZCYma2TJ1hIwR4ZkRQP0Uu7Cuq4k@redis-17407.c278.us-east-1-4.ec2.redns.redis-cloud.com:17407'
-})
-
-redisClient.on('error', (err) => console.error('Redis Client Error', err));
-
-// Connect to Redis
-redisClient.connect();
-
-
-// convert Redis INFO metrics to Prometheus format
-const prometheusFormat = (metrics: { [key: string]: string}): string => {
-    let output = '';
-    for (const [key, value] of Object.entries(metrics)) {
-        // Attempt to parse the value as a float
-        const floatValue = parseFloat(value);
-        if (!isNaN(floatValue)) {
-            output += `# TYPE redis_${key} gauge\n`;
-            output += `redis_${key} ${floatValue}\n`;
-        }
-    }
-    return output;
-}
+//     return output;
+// }
 
 // testing info command
 // async function getInfo() {
@@ -52,46 +45,38 @@ const prometheusFormat = (metrics: { [key: string]: string}): string => {
 
 
 // define the type for the metrics object
-interface Metrics {
-    [key: string]: string;
-}
-// prometheus server: 9090/metrics
-// route to get metrics from Redis
-app.get('/metrics', async (req, res) => {
-    try {
-        const info = await redisClient.info();
-        console.log('Redis INFO:', info); // Log the redis metrics using INFO command
+// interface Metrics {
+//     [key: string]: string;
+// }
+// // prometheus server: 9090/metrics
+// // route to get metrics from Redis
+// app.get('/metrics', async (req, res) => {
+//     try {
+//         const info = await redisClient.info();
+//         console.log('Redis INFO:', info); 
 
-        const metrics: Metrics = info.split('\n').reduce((acc: Metrics, line) => {
-            const parts = line.split(':');
-            if (parts.length === 2) {
-                acc[parts[0].trim()] = parts[1].trim();
-            }
-            return acc;
-        }, {});
+//         const metrics: Metrics = info.split('\n').reduce((acc: Metrics, line) => {
+//             const parts = line.split(':');
+//             if (parts.length === 2) {
+//                 acc[parts[0].trim()] = parts[1].trim();
+//             }
+//             return acc;
+//         }, {});
 
-        console.log('Parsed Metrics:', metrics);  // Log the parsed metrics
+//         console.log('Parsed Metrics:', metrics);  
 
-        const prometheusMetrics = prometheusFormat(metrics);
-        console.log('Prometheus Metrics:', prometheusMetrics);  // Log the Prometheus formatted metrics
+//         const prometheusMetrics = prometheusFormat(metrics);
+//         console.log('Prometheus Metrics:', prometheusMetrics);  
 
-        res.set('Content-Type', 'text/plain');
-        res.send(prometheusMetrics);
-    } catch (err) {
-        console.error('Error fetching metrics:', err);
-        res.status(500).json({ error: 'Failed to fetch metrics' });
-    }
-});
+//         res.set('Content-Type', 'text/plain');
+//         res.send(prometheusMetrics);
+//     } catch (err) {
+//         console.error('Error fetching metrics:', err);
+//         res.status(500).json({ error: 'Failed to fetch metrics' });
+//     }
+// });
 
-// GET route: get current scoreboard
-// app.get('/api', playerController.getScores, (req, res) =>
-//   res.json(res.locals.scores)
-// );
 
-// // POST route: update scoreboard
-// app.post('/api', playerController.updateScores, (req, res) =>
-//   res.json(res.locals.scores)
-// );
 
 // global error handler
 // app.use('/', (err, req, res, _next) => {
