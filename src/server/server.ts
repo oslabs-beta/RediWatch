bf819dfb3235ea6f65b7299e60a471579acaaf1f
Yoon Joo Woo <yjdream86@gmail.com>
2024-07-08 19:42:45 -0400

    tried to setup maxmemory

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
-import redis, { createClient } from 'redis';
+// import redis, { createClient, RedisClientType } from 'redis';
 import { performance } from 'perf_hooks';
+import Redis, { Redis as RedisClientType } from 'ioredis';
 // // prometheus library
 // import promClient from 'prom-client';
 
@@ -9,119 +10,139 @@ const PORT = 3001;
 
 app.use(express.json());
 
+interface PerformanceMetrics {
+    duration: number;
+    hits: number;
+    misses: number;
+}
 
+interface RedisConfigResult {
+    maxmemory: string;
+}
 
-// const redisClient = createClient({
-//     url: 'redis://default:J4qW0Kf6udoPHKmthBviCYfGXE5ommO2@redis-11565.c274.us-east-1-3.ec2.redns.redis-cloud.com:11565'
-// })
+const redisClient: RedisClientType = new Redis({
+    host: `redis-11565.c274.us-east-1-3.ec2.redns.redis-cloud.com`,
+    port: 11565,
+    password: 'J4qW0Kf6udoPHKmthBviCYfGXE5ommO2',
+    username: 'default',
+})
 
-// redisClient.on('error', (err) => console.error('Redis Client Error', err));
-
-// // Connect to Redis
-// redisClient.connect();
-
-
-// // convert Redis INFO metrics to Prometheus format
-// const prometheusFormat = (metrics: { [key: string]: string}): string => {
-//     let output = '';
-//     for (const [key, value] of Object.entries(metrics)) {
-//         // Attempt to parse the value as a float
-//         const floatValue = parseFloat(value);
-//         if (!isNaN(floatValue)) {
-//             output += `# TYPE redis_${key} gauge\n`;
-//             output += `redis_${key} ${floatValue}\n`;
-//         }
-//     }
-//     return output;
-// }
-
-// testing info command
-// async function getInfo() {
-//     try {
-//         const info = await redisClient.info();
-//         console.log(info);
-//     } catch (error) {
-//         console.error('Error fetching INFO from Redis', error);
-//     }
-// }
-// getInfo();
-
-
-// define the type for the metrics object
-// interface Metrics {
-//     [key: string]: string;
-// }
-// // prometheus server: 9090/metrics
-// // route to get metrics from Redis
-// app.get('/metrics', async (req, res) => {
-//     try {
-//         const info = await redisClient.info();
-//         console.log('Redis INFO:', info); 
-
-//         const metrics: Metrics = info.split('\n').reduce((acc: Metrics, line) => {
-//             const parts = line.split(':');
-//             if (parts.length === 2) {
-//                 acc[parts[0].trim()] = parts[1].trim();
-//             }
-//             return acc;
-//         }, {});
-
-//         console.log('Parsed Metrics:', metrics);  
-
-//         const prometheusMetrics = prometheusFormat(metrics);
-//         console.log('Prometheus Metrics:', prometheusMetrics);  
-
-//         res.set('Content-Type', 'text/plain');
-//         res.send(prometheusMetrics);
-//     } catch (err) {
-//         console.error('Error fetching metrics:', err);
-//         res.status(500).json({ error: 'Failed to fetch metrics' });
-//     }
-// });
+redisClient.on('error', (err) => console.error('Redis Client Error', err));
+redisClient.on('connect', () => console.log('Redis client connected'));
 
 app.post('/test', async (req, res) => {
-    const redisClient = createClient({
-        url: 'redis://default:J4qW0Kf6udoPHKmthBviCYfGXE5ommO2@redis-11565.c274.us-east-1-3.ec2.redns.redis-cloud.com:11565'
-    })
-    
-    redisClient.on('error', (err) => console.error('Redis Client Error', err));
-    redisClient.on('connect', () => console.log('Redis client connected'));
+    // const redisClient: RedisClientType = createClient({
+    //     url: 'redis://default:J4qW0Kf6udoPHKmthBviCYfGXE5ommO2@redis-11565.c274.us-east-1-3.ec2.redns.redis-cloud.com:11565'
+    // })
+   
     try {
         // Connect to Redis
-        await redisClient.connect();
+        await redisClient.ping();
         console.log('connected to Redis');
-
-        // add sample data to mimic cache behavior
-        const sampleData = Array.from({ length: 1000 }, (_, i) => `key${i}`);
-        // set ttl = 1 hour
-        const ttl = 60 * 60;
-
-        const start = performance.now();
-
-        for (const key of sampleData) {
-            await redisClient.set(key, `value${key}`, { EX: ttl });
-        }
-    
-        for (let i = 0; i < 2000; i++) {
-            const key = `key${Math.floor(Math.random() * 1000)}`;
-            await redisClient.get(key);
-        }
-
-        const end = performance.now();
-        // converting to seconds
-        const duration = (end - start) / 1000;
-        console.log('Duration: ', duration);
-
         
+        // Check supported configuration parameters
+        const supportedConfigs = await redisClient.config('GET', '*') as Array<string>;
+        console.log('Supported CONFIG parameters: ', supportedConfigs);
 
-        await redisClient.quit();
-        res.send({ duration });
+        if (supportedConfigs.includes('maxmemory')) {
+            // Set maxmemory to 30mb if supported
+            await redisClient.config('SET', 'maxmemory', '30mb');
+            console.log('maxmemory set to 30mb');
+
+            // Fetch maxmemory configuration
+            const configResult = await redisClient.config('GET', 'maxmemory') as Array<string>;
+            console.log('Config Result: ', configResult);
+
+            // Destructure the result to get the maxmemory value
+            const maxMemoryIndex = configResult.findIndex(config => config === 'maxmemory');
+            const maxMemory = maxMemoryIndex !== -1 ? configResult[maxMemoryIndex + 1] : undefined;
+            console.log('maxmemory: ', maxMemory);
+        } else {
+            console.log('maxmemory is not a supported CONFIG parameter.');
+        }
+
+        // Test cache performance
+        const performanceMetrics = await testCachePerformance(redisClient);
+        res.json(performanceMetrics);
+
+        // test each eviction methods
+        // const evictionPolicies = ['allkeys-lru', 'volatile-lru', 'allkeys-random', 'volatile-random', 'volatile-ttl', 'noeviction'];
+        
+        // for (const policy of evictionPolicies) {
+        //     console.log(`Testing ${policy}`);
+        //     const performanceMetrics = await testCachePerformance(redisClient, policy); 
+        //     console.log(`Performance metrics for policy ${policy}: `, performanceMetrics);
+            
+        // }
+        // const performanceMetrics = await testCachePerformance(redisClient);
+        // res.send(performanceMetrics);
+        
     } catch (error) {
         console.error('Error during Redis operations: ', error);
         res.status(500).send('Error during Redis operations');
     }    
 });
 
+
+
+async function testCachePerformance(redisClient: RedisClientType): Promise<PerformanceMetrics> {
+    // await redisClient.configSet('maxmemory', '30mb');
+    // await redisClient.configSet('maxmemory-policy', evictionPolicy);
+    // console.log('maxmemory reset');
+
+    // await redisClient.configGet('maxmemory', function(err: any, result: any) {
+    //     if (err) {
+    //         console.error('Error: ', err);
+    //     } else {
+    //         console.log('maxmemory: ', result[1]);
+    //     }
+    // })
+
+    // try {
+    //     const result = await redisClient.configGet('maxmemory');
+    //     const maxMemory = result['maxmemory'];
+    //     console.log('maxmemory: ', maxMemory);
+    // } catch (error) {
+    //     console.error('Error fetching maxmemory:', error);
+    // }
+
+    // add sample data to mimic cache behavior
+    const sampleData = Array.from({ length: 1000 }, (_, i) => `key${i}`);
+    // set ttl = 1 hour
+    const ttl = 60 * 60;
+
+    const start = performance.now();
+
+    // setting keys
+    for (const key of sampleData) {
+        await redisClient.set(key, `value${key}`, 'EX', ttl );
+    }
+    
+
+    // retrieve keys
+    let hits = 0;
+    let misses = 0;
+    for (let i = 0; i < 2000; i++) {
+        const key = `key${Math.floor(Math.random() * 1000)}`;
+        const result = await redisClient.get(key);
+        if (result !== null) {
+            hits++;
+        } else {
+            misses++;
+        }
+    }
+
+    const end = performance.now();
+    // converting to seconds
+    const duration = (end - start) / 1000;
+    console.log('Duration: ', duration);
+    console.log('hits: ', hits);
+    console.log('misses: ', misses);
+    
+
+    await redisClient.quit();
+    return { duration, hits, misses };
+}
 // global error handler
 // app.use('/', (err, req, res, _next) => {
 //   const defaultErr = {
