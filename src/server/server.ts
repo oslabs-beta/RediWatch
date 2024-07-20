b593de76e2352356e7b252f29d6e8857bcdd3a82
Yoon Joo Woo <yjdream86@gmail.com>
2024-07-20 17:54:04 -0400

    containerizing app

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,7 @@
-// import express, { Request, Response } from 'express';
-import { Client } from 'pg';
+import express, { Request, Response } from 'express';
+// import { Client } from 'pg';
 // @ts-nocheck
-const express = require('express');
+// const express = require('express');
 import { performance } from 'perf_hooks';
 import Redis, { Redis as RedisClientType } from 'ioredis';
 import 'dotenv/config';
